3c9990b7d5e0a5d0637a9719e04b7381fd576c22
Tanaphon Auanhinkong <tanaphon.auanhinkong@ifyoucan.com>
2018-06-18 13:33:16 +0700

    Passed TestSumAll

--- sum._test.go
+++ sum._test.go
@@ -1,15 +1,28 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3}
 
 	got := Sum(numbers)
-	want := 15
+	want := 6
 
-	if want != got {
+	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
--- sum.go
+++ sum.go
@@ -1,9 +1,20 @@
 package main
 
 // Sum - Sum all numbers in an array
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
+	}
+	return
+}
+
+// SumAll - SumAll numbers in arrays to new array
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumber := len(numbersToSum)
+	sums = make([]int, lengthOfNumber)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
 	}
 	return
 }
