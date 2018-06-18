a3345345ce028fbbac71cfe5efca87baee0dc135
Tanaphon Auanhinkong <tanaphon.auanhinkong@ifyoucan.com>
2018-06-18 15:20:13 +0700

    Refactor shapes parameters

--- shapes.go
+++ shapes.go
@@ -1,6 +1,17 @@
 package main
 
-// Perimeter - Calculate perimeter
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+// Rectangle - A rectangle struct
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+// Perimeter - Calculate perimeter of rectangle
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+// Area - Calcyulate area of rectangle
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
